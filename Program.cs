ffa4c346b5a3d9553579feeee5c5fe6b383a5f9e
nguyenhoanganhcntt <hoanganh140mhd@gmail.com>
2024-09-14 17:37:52 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bt1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soLuong;
+            decimal giaSanPham;
+            decimal tongGiaTriDonHang;
+            try
+            {
+
+                Console.Write("nhap so luong san pham : ");
+                soLuong = Int32.Parse(Console.ReadLine());
+
+                Console.Write("nhap gia cua mot san pham : ");
+                giaSanPham = decimal.Parse(Console.ReadLine());
+
+
+                tongGiaTriDonHang = soLuong * giaSanPham;
+                Console.Write("tong gia tri don hang : " + tongGiaTriDonHang);
+                Console.ReadLine();
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("nhap sai.");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("2280600079_NguyenHoangAnh")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2280600079_NguyenHoangAnh")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("95c3c31a-004d-4456-883c-42afc81e1e21")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
